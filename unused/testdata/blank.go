9e45cc9bc713f21d84156ec8cffb377c9b7923f4
Dominik Honnef <dominik@honnef.co>
2016-03-18 16:05:12 +0100

    Update tests

---
+++ unused/testdata/blank.go
@@ -0,0 +1,20 @@
+package pkg
+
+type t1 struct{} // MATCH t1
+type t2 struct{}
+type t3 struct{}
+
+var _ = t2{}
+
+func fn1() { // MATCH fn1
+	_ = t1{}
+	var _ = t1{}
+}
+
+func fn2() {
+	_ = t3{}
+}
+
+func init() {
+	fn2()
+}
