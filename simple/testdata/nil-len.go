3475496725e5b0cec51f76563c5fa31541efd7b2
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2016-04-02 10:59:09 +0800

    Suggest removing nil checks if followed by len check
    The idea was suggested at https://github.com/golang/lint/issues/195

---
+++ simple/testdata/nil-len.go
@@ -0,0 +1,35 @@
+package pkg
+
+func fn() {
+	var pa *[5]int
+	var s []int
+	var m map[int]int
+	var ch chan int
+
+	if s == nil || len(s) == 0 { // MATCH /should use len\(s\) == 0; nil check is redundant/
+	}
+	if m == nil || len(m) == 0 { // MATCH /should use len\(m\) == 0; nil check is redundant/
+	}
+	if ch == nil || len(ch) == 0 { // MATCH /should use len\(ch\) == 0; nil check is redundant/
+	}
+
+	if s != nil && len(s) != 0 { // MATCH /should use len\(s\) != 0; nil check is redundant/
+	}
+	if m != nil && len(m) > 0 { // MATCH /should use len\(m\) > 0; nil check is redundant/
+	}
+	if ch != nil && len(ch) == 5 { // MATCH /should use len\(ch\) == 5; nil check is redundant/
+	}
+
+	if pa == nil || len(pa) == 0 { // nil check cannot be removed with pointer to an array
+	}
+	if s == nil || len(m) == 0 { // different variables
+	}
+	if s != nil && len(m) == 1 { // different variables
+	}
+
+	var ch2 chan int
+	if ch == ch2 || len(ch) == 0 { // not comparing with nil
+	}
+	if ch != ch2 && len(ch) != 0 { // not comparing with nil
+	}
+}
