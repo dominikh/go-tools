d55a3d737ca38f4b80d61eddba9958b7dc6bbc81
Dominik Honnef <dominik@honnef.co>
2016-09-18 01:21:09 +0200

    Don't use IdenticalIgnoreTags yet, it's from the future
    Closes gh-24

--- simple/lint18.go
+++ simple/lint18.go
@@ -4,4 +4,6 @@ package simple
 
 import "go/types"
 
-var structsIdentical = types.IdenticalIgnoreTags
+// TODO(dh): use types.IdenticalIgnoreTags once CL 24190 has been merged
+// var structsIdentical = types.IdenticalIgnoreTags
+var structsIdentical = types.Identical
