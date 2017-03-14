7d73c00c5fef58e52b77724795b661c870bd83ff
Dominik Honnef <dominik@honnef.co>
2017-03-14 18:26:08 +0100

    simple: ignore struct tags in conversions for Go 1.8

--- simple/lint18.go
+++ simple/lint18.go
@@ -4,6 +4,4 @@ package simple
 
 import "go/types"
 
-// TODO(dh): use types.IdenticalIgnoreTags once CL 24190 has been merged
-// var structsIdentical = types.IdenticalIgnoreTags
-var structsIdentical = types.Identical
+var structsIdentical = types.IdenticalIgnoreTags
