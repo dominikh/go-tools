640d07ea208beb53ce3ae175d9bc5e2531dc5729
Stuart Carnie <stuart.carnie@gmail.com>
2017-07-18 07:36:41 -0700

    add matches

--- staticcheck/testdata/CheckTimeEqualityComparison.go
+++ staticcheck/testdata/CheckTimeEqualityComparison.go
@@ -7,21 +7,21 @@ import (
 func fn1() {
 	var t1, t2 time.Time
 
-	_ = t1 == t2
-	_ = t1 != t2
+	_ = t1 == t2 // MATCH /using == or != operators to compare time is problematic/
+	_ = t1 != t2 // MATCH /using == or != operators to compare time is problematic/
 }
 
 func fn2() {
 	var t1, t2 *time.Time
 
-	_ = *t1 == *t2
-	_ = *t1 != *t2
+	_ = *t1 == *t2 // MATCH /using == or != operators to compare time is problematic/
+	_ = *t1 != *t2 // MATCH /using == or != operators to compare time is problematic/
 }
 
 func fn3() {
 	var t1 time.Time
 	var t2 *time.Time
 
-	_ = t1 == *t2
-	_ = t1 != *t2
+	_ = t1 == *t2 // MATCH /using == or != operators to compare time is problematic/
+	_ = t1 != *t2 // MATCH /using == or != operators to compare time is problematic/
 }
