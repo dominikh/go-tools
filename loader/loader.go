b545bdaf688df6b79cd61349e51305bc25ac7518
Dominik Honnef <dominik@honnef.co>
2018-02-18 03:00:36 +0100

    loader: clean up API

--- loader/loader.go
+++ loader/loader.go
@@ -9,23 +9,11 @@ import (
 	"go/types"
 	"path/filepath"
 	"sync"
-	"sync/atomic"
-	"time"
 
 	"honnef.co/go/tools/ssa"
 )
 
-type Statistics struct {
-	Finding      time.Duration
-	Parsing      time.Duration
-	TypeChecking time.Duration
-	SSA          time.Duration
-}
-
-func (stat Statistics) String() string {
-	return fmt.Sprintf("finding = %s, parsing = %s, type checking = %s, SSA = %s",
-		stat.Finding, stat.Parsing, stat.TypeChecking, stat.SSA)
-}
+// XXX make Import safe for concurrent use
 
 type Program struct {
 	Fset   *token.FileSet
@@ -33,13 +21,36 @@ type Program struct {
 	Config *types.Config
 	SSA    *ssa.Program
 
-	Packages   map[string]*Package
-	Statistics Statistics
+	Packages map[string]*Package
 
 	unsafe *Package
 
 	bpkgsMu sync.Mutex
 	bpkgs   map[string]*bpkg
+
+	TokenFileMap map[*token.File]*ast.File
+	ASTFileMap   map[*ast.File]*Package
+}
+
+func NewProgram() *Program {
+	fset := token.NewFileSet()
+	ssaprog := ssa.NewProgram(fset, ssa.GlobalDebug)
+	prog := &Program{
+		Fset:     fset,
+		Build:    &build.Default,
+		Config:   &types.Config{},
+		SSA:      ssaprog,
+		Packages: map[string]*Package{},
+		unsafe: &Package{
+			Pkg: types.Unsafe,
+			SSA: ssaprog.CreatePackage(types.Unsafe, nil, nil, true),
+		},
+		bpkgs:        map[string]*bpkg{},
+		TokenFileMap: map[*token.File]*ast.File{},
+		ASTFileMap:   map[*ast.File]*Package{},
+	}
+	prog.Config.Importer = importer{prog}
+	return prog
 }
 
 type Package struct {
@@ -48,21 +59,34 @@ type Package struct {
 	SSA   *ssa.Package
 	Files []*ast.File
 	types.Info
+
+	augmented bool
 }
 
-func (prog *Program) Init() {
-	prog.Config.Importer = importer{prog}
-	prog.Packages = map[string]*Package{}
-	prog.unsafe = &Package{
-		Pkg: types.Unsafe,
-		SSA: prog.SSA.CreatePackage(types.Unsafe, nil, nil, true),
+func (pkg *Package) String() string {
+	return fmt.Sprintf("package %s // import %q", pkg.Pkg.Name(), pkg.Pkg.Path())
+}
+
+const (
+	goFiles = iota
+	testFiles
+	xtestFiles
+)
+
+func (prog *Program) parsePackage(bpkg *build.Package, which int) ([]*ast.File, error) {
+	var in []string
+	switch which {
+	case goFiles:
+		in = bpkg.GoFiles
+	case testFiles:
+		in = bpkg.TestGoFiles
+	case xtestFiles:
+		in = bpkg.XTestGoFiles
+	default:
+		panic(fmt.Sprintf("invalid value for which: %d", which))
 	}
-	prog.bpkgs = map[string]*bpkg{}
-}
-
-func (prog *Program) parsePackage(bpkg *build.Package) ([]*ast.File, error) {
-	files := make([]*ast.File, len(bpkg.GoFiles))
-	for i, name := range bpkg.GoFiles {
+	files := make([]*ast.File, len(in))
+	for i, name := range in {
 		path := filepath.Join(bpkg.Dir, name)
 		f, err := parser.ParseFile(prog.Fset, path, nil, parser.ParseComments)
 		if err != nil {
@@ -74,16 +98,18 @@ func (prog *Program) parsePackage(bpkg *build.Package) ([]*ast.File, error) {
 }
 
 type bpkg struct {
-	bp    *build.Package
-	files []*ast.File
-	err   error
-	ready chan struct{} // closed to broadcast readiness
+	bp         *build.Package
+	files      []*ast.File
+	testFiles  []*ast.File
+	xtestFiles []*ast.File
+	err        error
+	ready      chan struct{} // closed to broadcast readiness
 }
 
-func (prog *Program) findPackage(path, dir string) (*build.Package, []*ast.File, error) {
+func (prog *Program) findPackage(path, dir string) (*bpkg, error) {
 	bp, err := prog.Build.Import(path, dir, build.FindOnly)
 	if err != nil {
-		return bp, nil, err
+		return nil, err
 	}
 	prog.bpkgsMu.Lock()
 	v, ok := prog.bpkgs[bp.ImportPath]
@@ -92,38 +118,65 @@ func (prog *Program) findPackage(path, dir string) (*build.Package, []*ast.File,
 		<-v.ready
 	} else {
 		v = &bpkg{ready: make(chan struct{})}
+		defer close(v.ready)
 		prog.bpkgs[bp.ImportPath] = v
 		prog.bpkgsMu.Unlock()
 
 		v.bp, v.err = prog.Build.Import(path, dir, 0)
 		if v.err == nil {
-			t := time.Now()
-			v.files, v.err = prog.parsePackage(v.bp)
-			d := time.Since(t)
-			atomic.AddInt64((*int64)(&prog.Statistics.Parsing), int64(d))
+			v.files, v.err = prog.parsePackage(v.bp, goFiles)
+			if v.err != nil {
+				return v, v.err
+			}
+			v.testFiles, v.err = prog.parsePackage(v.bp, testFiles)
+			if v.err != nil {
+				return v, v.err
+			}
+			v.xtestFiles, v.err = prog.parsePackage(v.bp, xtestFiles)
+			if v.err != nil {
+				return v, v.err
+			}
 		}
-		close(v.ready)
 	}
-	return v.bp, v.files, v.err
+	return v, v.err
 }
 
-func (prog *Program) Import(path string, cwd string) (*Package, error) {
+func (prog *Program) Import(path string, cwd string, tests bool) (*Package, error) {
 	pkg, err := prog.load(path, cwd)
 	if err != nil {
 		return nil, err
 	}
-	t := time.Now()
+	if tests && !pkg.augmented {
+		// XXX augment import with tests
+		//
+		// XXX what to do about SSA? if the package was already
+		// imported without tests, SSA building will have already
+		// finished.
+		//
+		// XXX also need to add test files to pkg.Files
+	}
 	prog.SSA.Build()
-	prog.Statistics.SSA += time.Since(t)
 	return pkg, nil
 }
 
-func (prog *Program) load(path string, cwd string) (*Package, error) {
-	if path == "unsafe" {
-		return prog.unsafe, nil
+func (prog *Program) CreateFromFiles(path string, files ...*ast.File) (*Package, error) {
+	// prefetch build.Packages of dependencies
+	for _, f := range files {
+		for _, imp := range f.Imports {
+			go prog.findPackage(imp.Path.Value, "")
+		}
 	}
+	// if tests {
+	// 	for _, imp := range bpkg.bp.TestImports {
+	// 		go prog.findPackage(imp, bpkg.bp.Dir)
+	// 	}
+	// 	for _, imp := range bpkg.bp.XTestImports {
+	// 		go prog.findPackage(imp, bpkg.bp.Dir)
+	// 	}
+	// }
 
-	pkg := Package{
+	pkgPath := path
+	pkg := &Package{
 		Info: types.Info{
 			Types:      map[ast.Expr]types.TypeAndValue{},
 			Defs:       map[*ast.Ident]types.Object{},
@@ -133,35 +186,86 @@ func (prog *Program) load(path string, cwd string) (*Package, error) {
 			Scopes:     map[ast.Node]*types.Scope{},
 			InitOrder:  []*types.Initializer{},
 		},
-	}
-	var err error
-	t := time.Now()
-	pkg.Bpkg, pkg.Files, err = prog.findPackage(path, cwd)
-	if err != nil {
-		return nil, err
+		Bpkg:  nil,
+		Pkg:   types.NewPackage(pkgPath, ""),
+		Files: files,
 	}
 
-	if c, ok := prog.Packages[pkg.Bpkg.ImportPath]; ok {
+	c := types.NewChecker(prog.Config, prog.Fset, pkg.Pkg, &pkg.Info)
+	if err := c.Files(files); err != nil {
+		return nil, err
+	}
+	pkg.SSA = prog.SSA.CreatePackage(pkg.Pkg, pkg.Files, &pkg.Info, true)
+	pkg.SSA.Build()
+	// prog.Packages[bpkg.bp.ImportPath] = pkg
+
+	for _, f := range pkg.Files {
+		tf := prog.Fset.File(f.Pos())
+		prog.TokenFileMap[tf] = f
+		prog.ASTFileMap[f] = pkg
+	}
+	return pkg, nil
+}
+
+func (prog *Program) createFromBpkg(bpkg *bpkg) (*Package, error) {
+	if c, ok := prog.Packages[bpkg.bp.ImportPath]; ok {
 		return c, nil
 	}
-	prog.Statistics.Finding += time.Since(t)
 
-	for _, imp := range pkg.Bpkg.Imports {
-		// prefetch build.Packages of dependencies
-		go prog.findPackage(imp, pkg.Bpkg.Dir)
+	// prefetch build.Packages of dependencies
+	for _, imp := range bpkg.bp.Imports {
+		go prog.findPackage(imp, bpkg.bp.Dir)
+	}
+	// if tests {
+	// 	for _, imp := range bpkg.bp.TestImports {
+	// 		go prog.findPackage(imp, bpkg.bp.Dir)
+	// 	}
+	// 	for _, imp := range bpkg.bp.XTestImports {
+	// 		go prog.findPackage(imp, bpkg.bp.Dir)
+	// 	}
+	// }
+
+	pkgPath := bpkg.bp.ImportPath
+	pkg := &Package{
+		Info: types.Info{
+			Types:      map[ast.Expr]types.TypeAndValue{},
+			Defs:       map[*ast.Ident]types.Object{},
+			Uses:       map[*ast.Ident]types.Object{},
+			Implicits:  map[ast.Node]types.Object{},
+			Selections: map[*ast.SelectorExpr]*types.Selection{},
+			Scopes:     map[ast.Node]*types.Scope{},
+			InitOrder:  []*types.Initializer{},
+		},
+		Bpkg:  bpkg.bp,
+		Pkg:   types.NewPackage(pkgPath, ""),
+		Files: bpkg.files,
 	}
 
-	pkg.Pkg, err = prog.Config.Check(pkg.Bpkg.ImportPath, prog.Fset, pkg.Files, &pkg.Info)
+	c := types.NewChecker(prog.Config, prog.Fset, pkg.Pkg, &pkg.Info)
+	if err := c.Files(bpkg.files); err != nil {
+		return nil, err
+	}
+	pkg.SSA = prog.SSA.CreatePackage(pkg.Pkg, pkg.Files, &pkg.Info, true)
+	prog.Packages[bpkg.bp.ImportPath] = pkg
+
+	for _, f := range pkg.Files {
+		tf := prog.Fset.File(f.Pos())
+		prog.TokenFileMap[tf] = f
+		prog.ASTFileMap[f] = pkg
+	}
+	return pkg, nil
+}
+
+func (prog *Program) load(path string, cwd string) (*Package, error) {
+	if path == "unsafe" {
+		return prog.unsafe, nil
+	}
+
+	bpkg, err := prog.findPackage(path, cwd)
 	if err != nil {
 		return nil, err
 	}
-
-	t = time.Now()
-	pkg.SSA = prog.SSA.CreatePackage(pkg.Pkg, pkg.Files, &pkg.Info, true)
-	prog.Statistics.SSA += time.Since(t)
-
-	prog.Packages[pkg.Bpkg.ImportPath] = &pkg
-	return &pkg, nil
+	return prog.createFromBpkg(bpkg)
 }
 
 type importer struct {
