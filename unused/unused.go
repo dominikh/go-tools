dominikh/go-tools

commit e63a39a5b848a1b45f42e36b4e49d8ed3fa69d66

unused: don't discard cross-package edges in whole program mode
Closes gh-443

unused/unused.go
@@ -861,14 +861,16 @@ func (g *Graph) use(used, by interface{}, reason string) {
 	if _, ok := by.(*types.Func); ok {
 		assert(g.pkg.Prog.FuncValue(by.(*types.Func)) == nil)
 	}
-	if obj, ok := used.(types.Object); ok && obj.Pkg() != nil {
-		if obj.Pkg() != g.pkg.Pkg {
-			return
+	if !g.wholeProgram {
+		if obj, ok := used.(types.Object); ok && obj.Pkg() != nil {
+			if obj.Pkg() != g.pkg.Pkg {
+				return
+			}
 		}
-	}
-	if obj, ok := by.(types.Object); ok && obj.Pkg() != nil {
-		if obj.Pkg() != g.pkg.Pkg {
-			return
+		if obj, ok := by.(types.Object); ok && obj.Pkg() != nil {
+			if obj.Pkg() != g.pkg.Pkg {
+				return
+			}
 		}
 	}
 	usedNode, new := g.node(used)

commit 5f558939d3aafbdade56e68e14c4f6b4c2df786e

unused: fix handling of packages in whole program mode
e63a39a5b848a1b45f42e36b4e49d8ed3fa69d66 introduced an incorrect fix for
whole program mode. The fix was incorrect because it assumed that all
packages were relevant. However, even in whole program mode, we do not
care about dependencies that aren't being explicitly checked.

unused/unused.go
@@ -833,6 +833,18 @@ func isIrrelevant(obj interface{}) bool {
 	return false
 }
 
+func (g *Graph) isInterestingPackage(pkg *types.Package) bool {
+	if g.wholeProgram {
+		for _, opkg := range g.job.Program.InitialPackages {
+			if opkg.Types == pkg {
+				return true
+			}
+		}
+		return false
+	}
+	return pkg == g.pkg.Pkg
+}
+
 func (g *Graph) see(obj interface{}) {
 	if isIrrelevant(obj) {
 		return
@@ -840,7 +852,7 @@ func (g *Graph) see(obj interface{}) {
 
 	assert(obj != nil)
 	if obj, ok := obj.(types.Object); ok && obj.Pkg() != nil {
-		if obj.Pkg() != g.pkg.Pkg {
+		if !g.isInterestingPackage(obj.Pkg()) {
 			return
 		}
 	}
@@ -861,16 +873,14 @@ func (g *Graph) use(used, by interface{}, reason string) {
 	if _, ok := by.(*types.Func); ok {
 		assert(g.pkg.Prog.FuncValue(by.(*types.Func)) == nil)
 	}
-	if !g.wholeProgram {
-		if obj, ok := used.(types.Object); ok && obj.Pkg() != nil {
-			if obj.Pkg() != g.pkg.Pkg {
-				return
-			}
+	if obj, ok := used.(types.Object); ok && obj.Pkg() != nil {
+		if !g.isInterestingPackage(obj.Pkg()) {
+			return
 		}
-		if obj, ok := by.(types.Object); ok && obj.Pkg() != nil {
-			if obj.Pkg() != g.pkg.Pkg {
-				return
-			}
+	}
+	if obj, ok := by.(types.Object); ok && obj.Pkg() != nil {
+		if !g.isInterestingPackage(obj.Pkg()) {
+			return
 		}
 	}
 	usedNode, new := g.node(used)