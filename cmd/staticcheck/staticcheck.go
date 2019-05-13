c6fa781e62570f5844fcf7ee0a156d49c11a1560
Dominik Honnef <dominik@honnef.co>
2019-05-13 21:50:59 +0200

    cmd/staticcheck: add flag for dumping 'unused' debug graph

--- cmd/staticcheck/staticcheck.go
+++ cmd/staticcheck/staticcheck.go
@@ -2,6 +2,7 @@
 package main // import "honnef.co/go/tools/cmd/staticcheck"
 
 import (
+	"log"
 	"os"
 
 	"golang.org/x/tools/go/analysis"
@@ -16,6 +17,7 @@ import (
 func main() {
 	fs := lintutil.FlagSet("staticcheck")
 	wholeProgram := fs.Bool("unused.whole-program", false, "Run unused in whole program mode")
+	debug := fs.String("debug.unused-graph", "", "Write unused's object graph to `file`")
 	fs.Parse(os.Args[1:])
 
 	var cs []*analysis.Analyzer
@@ -30,6 +32,13 @@ func main() {
 	}
 
 	u := unused.NewChecker(*wholeProgram)
+	if *debug != "" {
+		f, err := os.OpenFile(*debug, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		u.Debug = f
+	}
 	cums := []lint.CumulativeChecker{u}
 	lintutil.ProcessFlagSet(cs, cums, fs)
 }
