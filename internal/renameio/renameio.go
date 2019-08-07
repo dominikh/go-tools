4bcd6d5dd648c00f66244f8873290539680e512a
Dominik Honnef <dominik@honnef.co>
2019-08-07 06:20:32 +0200

    internal/renameio: add a ReadFile function
    This backports the following upstream commit:
    
    commit bf1f4ec7fa3938e1ce1297b367c16aea30280697
    Author: Bryan C. Mills <bcmills@google.com>
    Date:   Fri May 31 17:22:13 2019 -0400
    
        cmd/go/internal/renameio: add a ReadFile function
    
        ReadFile is a drop-in replacement for ioutil.ReadFile that works
        around Windows filesystem flakiness under load.
    
        A followup CL will replace uses of ioutil.ReadFile in cmd/go with this
        function.
    
        Updates #32188
    
        Change-Id: I232ba893b132bdc84cd7b0edde436165a69e1aa8
        Reviewed-on: https://go-review.googlesource.com/c/go/+/180219
        Run-TryBot: Bryan C. Mills <bcmills@google.com>
        TryBot-Result: Gobot Gobot <gobot@golang.org>
        Reviewed-by: Jay Conrod <jayconrod@google.com>

--- internal/renameio/renameio.go
+++ internal/renameio/renameio.go
@@ -12,7 +12,6 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
-	"time"
 )
 
 const patternSuffix = ".tmp"
@@ -62,23 +61,7 @@ func WriteToFile(filename string, data io.Reader, perm os.FileMode) (err error) 
 		return err
 	}
 
-	var start time.Time
-	for {
-		err := os.Rename(f.Name(), filename)
-		if err == nil || !isAccessDeniedError(err) {
-			return err
-		}
-
-		// Windows seems to occasionally trigger spurious "Access is denied" errors
-		// here (see golang.org/issue/31247). We're not sure why. It's probably
-		// worth a little extra latency to avoid propagating the spurious errors.
-		if start.IsZero() {
-			start = time.Now()
-		} else if time.Since(start) >= 500*time.Millisecond {
-			return err
-		}
-		time.Sleep(5 * time.Millisecond)
-	}
+	return rename(f.Name(), filename)
 }
 
 // tempFile creates a new temporary file with given permission bits.
@@ -92,4 +75,17 @@ func tempFile(dir, prefix string, perm os.FileMode) (f *os.File, err error) {
 		break
 	}
 	return
+}
+
+// ReadFile is like ioutil.ReadFile, but on Windows retries spurious errors that
+// may occur if the file is concurrently replaced.
+//
+// Errors are classified heuristically and retries are bounded, so even this
+// function may occasionally return a spurious error on Windows.
+// If so, the error will likely wrap one of:
+//     - syscall.ERROR_ACCESS_DENIED
+//     - syscall.ERROR_FILE_NOT_FOUND
+//     - internal/syscall/windows.ERROR_SHARING_VIOLATION
+func ReadFile(filename string) ([]byte, error) {
+	return readFile(filename)
 }
