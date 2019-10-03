dominikh/go-tools

commit d48d6430c341431283deaf5ef49e345c4d34db16

ssa: make Const an instruction

ssa/emit.go
@@ -9,6 +9,7 @@ package ssa
 import (
 	"fmt"
 	"go/ast"
+	"go/constant"
 	"go/token"
 	"go/types"
 )
@@ -144,7 +145,7 @@ func emitCompare(f *Function, op token.Token, x, y Value, pos token.Pos) Value {
 	//   if e==true { ... }
 	// even in the case when e's type is an interface.
 	// TODO(adonovan): opt: generalise to x==true, false!=y, etc.
-	if x == vTrue && op == token.EQL {
+	if x, ok := x.(*Const); ok && op == token.EQL && x.Value.Kind() == constant.Bool && constant.BoolVal(x.Value) {
 		if yt, ok := yt.(*types.Basic); ok && yt.Info()&types.IsBoolean != 0 {
 			return y
 		}
@@ -233,7 +234,7 @@ func emitConv(f *Function, val Value, typ types.Type) Value {
 
 		// Untyped nil constant?  Return interface-typed nil constant.
 		if ut_src == tUntypedNil {
-			return nilConst(typ)
+			return emitConst(f, nilConst(typ))
 		}
 
 		// Convert (non-nil) "untyped" literals to their default type.
@@ -255,7 +256,7 @@ func emitConv(f *Function, val Value, typ types.Type) Value {
 			// constant of the destination type and
 			// (initially) the same abstract value.
 			// We don't truncate the value yet.
-			return NewConst(c.Value, typ)
+			return emitConst(f, NewConst(c.Value, typ))
 		}
 
 		// We're converting from constant to non-constant type,
@@ -476,8 +477,13 @@ func zeroValue(f *Function, t types.Type) Value {
 	case *types.Struct, *types.Array:
 		return emitLoad(f, f.addLocal(t, token.NoPos))
 	default:
-		return zeroConst(t)
+		return emitConst(f, zeroConst(t))
 	}
+}
+
+func emitConst(f *Function, c *Const) *Const {
+	f.consts = append(f.consts, c)
+	return c
 }
 
 // createRecoverBlock emits to f a block of code to return after a

functions/pure.go
@@ -4,43 +4,21 @@ import (
 	"honnef.co/go/tools/ssa"
 )
 
-func filterDebug(instr []ssa.Instruction) []ssa.Instruction {
-	var out []ssa.Instruction
-	for _, ins := range instr {
-		if _, ok := ins.(*ssa.DebugRef); !ok {
-			out = append(out, ins)
+// IsStub reports whether a function is a stub. A function is
+// considered a stub if it has no instructions or if all it does is
+// return a constant value.
+func IsStub(fn *ssa.Function) bool {
+	for _, b := range fn.Blocks {
+		for _, instr := range b.Instrs {
+			switch instr.(type) {
+			case *ssa.InitMem, *ssa.Const, *ssa.Panic, *ssa.Return, *ssa.DebugRef:
+				// InitMem and Const have no side-effects, Panic and
+				// Return must be using a constant value, or there are
+				// other instructions.
+			default:
+				return false
+			}
 		}
 	}
-	return out
-}
-
-// IsStub reports whether a function is a stub. A function is
-// considered a stub if it has no instructions or exactly two
-// instruction, InitMem and a return, which must be either returning only constant values or
-// a panic.
-func IsStub(fn *ssa.Function) bool {
-	if len(fn.Blocks) == 0 {
-		return true
-	}
-	if len(fn.Blocks) > 1 {
-		return false
-	}
-	instrs := filterDebug(fn.Blocks[0].Instrs)
-	if len(instrs) != 2 {
-		return false
-	}
-
-	switch instrs[1].(type) {
-	case *ssa.Return:
-		// Since this is the only instruction, the return value must
-		// be a constant. We consider all constants as stubs, not just
-		// the zero value. This does not, unfortunately, cover zero
-		// initialised structs, as these cause additional
-		// instructions.
-		return true
-	case *ssa.Panic:
-		return true
-	default:
-		return false
-	}
+	return true
 }

ssa/builder.go
@@ -59,11 +59,6 @@ var (
 	tUntypedNil = types.Typ[types.UntypedNil]
 	tRangeIter  = &opaqueType{nil, "iter"} // the type of all "range" iterators
 	tEface      = types.NewInterfaceType(nil, nil).Complete()
-
-	// SSA Value constants.
-	vZero = intConst(0)
-	vOne  = intConst(1)
-	vTrue = NewConst(constant.MakeBool(true), tBool)
 )
 
 // builder holds state associated with the package currently being built.
@@ -131,11 +126,11 @@ func (b *builder) logicalBinop(fn *Function, e *ast.BinaryExpr) Value {
 	switch e.Op {
 	case token.LAND:
 		b.cond(fn, e.X, rhs, done)
-		short = NewConst(constant.MakeBool(false), t)
+		short = emitConst(fn, NewConst(constant.MakeBool(false), t))
 
 	case token.LOR:
 		b.cond(fn, e.X, done, rhs)
-		short = NewConst(constant.MakeBool(true), t)
+		short = emitConst(fn, NewConst(constant.MakeBool(true), t))
 	}
 
 	// Is rhs unreachable?
@@ -272,7 +267,7 @@ func (b *builder) builtin(fn *Function, obj *types.Builtin, args []ast.Expr, typ
 			return fn.emit(v)
 
 		case *types.Chan:
-			var sz Value = vZero
+			var sz Value = emitConst(fn, intConst(0))
 			if len(args) == 2 {
 				sz = b.expr(fn, args[1])
 			}
@@ -296,7 +291,7 @@ func (b *builder) builtin(fn *Function, obj *types.Builtin, args []ast.Expr, typ
 		t := deref(fn.Pkg.typeOf(args[0])).Underlying()
 		if at, ok := t.(*types.Array); ok {
 			b.expr(fn, args[0]) // for effects only
-			return intConst(at.Len())
+			return emitConst(fn, intConst(at.Len()))
 		}
 		// Otherwise treat as normal.
 
@@ -306,7 +301,7 @@ func (b *builder) builtin(fn *Function, obj *types.Builtin, args []ast.Expr, typ
 			pos: pos,
 		})
 		fn.currentBlock = fn.newBasicBlock("unreachable")
-		return vTrue // any non-nil Value will do
+		return emitConst(fn, NewConst(constant.MakeBool(true), tBool)) // any non-nil Value will do
 	}
 	return nil // treat all others as a regular function call
 }
@@ -517,7 +512,7 @@ func (b *builder) expr(fn *Function, e ast.Expr) Value {
 
 	// Is expression a constant?
 	if tv.Value != nil {
-		return NewConst(tv.Value, tv.Type)
+		return emitConst(fn, NewConst(tv.Value, tv.Type))
 	}
 
 	var v Value
@@ -678,7 +673,7 @@ func (b *builder) expr0(fn *Function, e ast.Expr, tv types.TypeAndValue) Value {
 		case *types.Builtin:
 			return &Builtin{name: obj.Name(), sig: tv.Type.(*types.Signature)}
 		case *types.Nil:
-			return nilConst(tv.Type)
+			return emitConst(fn, nilConst(tv.Type))
 		}
 		// Package-level func or var?
 		if v := fn.Prog.packageLevelValue(obj); v != nil {
@@ -929,7 +924,7 @@ func (b *builder) emitCallArgs(fn *Function, sig *types.Signature, e *ast.CallEx
 		st := sig.Params().At(np).Type().(*types.Slice)
 		vt := st.Elem()
 		if len(varargs) == 0 {
-			args = append(args, nilConst(st))
+			args = append(args, emitConst(fn, nilConst(st)))
 		} else {
 			// Replace a suffix of args with a slice containing it.
 			at := types.NewArray(vt, int64(len(varargs)))
@@ -939,7 +934,7 @@ func (b *builder) emitCallArgs(fn *Function, sig *types.Signature, e *ast.CallEx
 			for i, arg := range varargs {
 				iaddr := &IndexAddr{
 					X:     a,
-					Index: intConst(int64(i)),
+					Index: emitConst(fn, intConst(int64(i))),
 				}
 				iaddr.setType(types.NewPointer(vt))
 				fn.emit(iaddr)
@@ -1163,7 +1158,7 @@ func (b *builder) compLit(fn *Function, addr Value, e *ast.CompositeLit, isZero 
 				if idx != nil {
 					idxval = idx.Int64() + 1
 				}
-				idx = intConst(idxval)
+				idx = emitConst(fn, intConst(idxval))
 			}
 			iaddr := &IndexAddr{
 				X:     array,
@@ -1187,7 +1182,7 @@ func (b *builder) compLit(fn *Function, addr Value, e *ast.CompositeLit, isZero 
 		}
 
 	case *types.Map:
-		m := &MakeMap{Reserve: intConst(int64(len(e.Elts)))}
+		m := &MakeMap{Reserve: emitConst(fn, intConst(int64(len(e.Elts))))}
 		m.setPos(e.Lbrace)
 		m.setType(typ)
 		fn.emit(m)
@@ -1242,7 +1237,7 @@ func (b *builder) switchStmt(fn *Function, s *ast.SwitchStmt, label *lblock) {
 	if s.Init != nil {
 		b.stmt(fn, s.Init)
 	}
-	var tag Value = vTrue
+	var tag Value = emitConst(fn, NewConst(constant.MakeBool(true), tBool))
 	if s.Tag != nil {
 		tag = b.expr(fn, s.Tag)
 	}
@@ -1399,7 +1394,7 @@ func (b *builder) typeSwitchStmt(fn *Function, s *ast.TypeSwitchStmt, label *lbl
 			casetype = fn.Pkg.typeOf(cond)
 			var condv Value
 			if casetype == tUntypedNil {
-				condv = emitCompare(fn, token.EQL, x, nilConst(x.Type()), token.NoPos)
+				condv = emitCompare(fn, token.EQL, x, emitConst(fn, nilConst(x.Type())), token.NoPos)
 				ti = x
 			} else {
 				yok := emitTypeTest(fn, x, casetype, cc.Case)
@@ -1565,7 +1560,7 @@ func (b *builder) selectStmt(fn *Function, s *ast.SelectStmt, label *lblock) {
 		}
 		body := fn.newBasicBlock("select.body")
 		next := fn.newBasicBlock("select.next")
-		emitIf(fn, emitCompare(fn, token.EQL, idx, intConst(int64(state)), token.NoPos), body, next)
+		emitIf(fn, emitCompare(fn, token.EQL, idx, emitConst(fn, intConst(int64(state))), token.NoPos), body, next)
 		fn.currentBlock = body
 		fn.targets = &targets{
 			tail:   fn.targets,
@@ -1616,7 +1611,7 @@ func (b *builder) selectStmt(fn *Function, s *ast.SelectStmt, label *lblock) {
 		// A blocking select must match some case.
 		// (This should really be a runtime.errorString, not a string.)
 		fn.emit(&Panic{
-			X: emitConv(fn, stringConst("blocking select matched no case"), tEface),
+			X: emitConv(fn, emitConst(fn, stringConst("blocking select matched no case")), tEface),
 		})
 		fn.currentBlock = fn.newBasicBlock("unreachable")
 	}
@@ -1707,7 +1702,7 @@ func (b *builder) rangeIndexed(fn *Function, x Value, tv types.Type, pos token.P
 		// elimination if x is pure, static unrolling, etc.
 		// Ranging over a nil *array may have >0 iterations.
 		// We still generate code for x, in case it has effects.
-		length = intConst(arr.Len())
+		length = emitConst(fn, intConst(arr.Len()))
 	} else {
 		// length = len(x).
 		var c Call
@@ -1718,7 +1713,7 @@ func (b *builder) rangeIndexed(fn *Function, x Value, tv types.Type, pos token.P
 	}
 
 	index := fn.addLocal(tInt, token.NoPos)
-	emitStore(fn, index, intConst(-1), pos)
+	emitStore(fn, index, emitConst(fn, intConst(-1)), pos)
 
 	loop = fn.newBasicBlock("rangeindex.loop")
 	emitJump(fn, loop)
@@ -1727,7 +1722,7 @@ func (b *builder) rangeIndexed(fn *Function, x Value, tv types.Type, pos token.P
 	incr := &BinOp{
 		Op: token.ADD,
 		X:  emitLoad(fn, index),
-		Y:  vOne,
+		Y:  emitConst(fn, intConst(1)),
 	}
 	incr.setType(tInt)
 	emitStore(fn, index, fn.emit(incr), pos)
@@ -1998,7 +1993,7 @@ start:
 			op = token.SUB
 		}
 		loc := b.addr(fn, s.X, false)
-		b.assignOp(fn, loc, NewConst(constant.MakeInt64(1), loc.typ()), op, s.Pos())
+		b.assignOp(fn, loc, emitConst(fn, NewConst(constant.MakeInt64(1), loc.typ())), op, s.Pos())
 
 	case *ast.AssignStmt:
 		switch s.Tok {
@@ -2299,7 +2294,7 @@ func (p *Package) build() {
 	done = init.newBasicBlock("init.done")
 	emitIf(init, emitLoad(init, initguard), done, doinit)
 	init.currentBlock = doinit
-	emitStore(init, initguard, vTrue, token.NoPos)
+	emitStore(init, initguard, emitConst(init, NewConst(constant.MakeBool(true), tBool)), token.NoPos)
 
 	// Call the init() function of each package we import.
 	for _, pkg := range p.Pkg.Imports() {

ssa/builder_test.go
@@ -260,13 +260,15 @@ func TestInit(t *testing.T) {
 # Synthetic: package initializer
 func init():
 0:                                                                entry P:0 S:2
-	t1 = InitMem                                                     Memory
-	t2 = Load <bool> init$guard t1                                     bool
-	if t2 goto 2 else 1
+	t1 = true:bool                                                     bool
+	t2 = 42:int                                                         int
+	t3 = InitMem                                                     Memory
+	t4 = Load <bool> init$guard t3                                     bool
+	if t4 goto 2 else 1
 1:                                                           init.start P:1 S:1
-	t5 = Store <mem> {bool} init$guard true:bool t1                  Memory
-	t6 = call errors.init() t5                                       Memory
-	t8 = Store <mem> {int} i 42:int t6                               Memory
+	t6 = Store <mem> {bool} init$guard true:bool t3                  Memory
+	t7 = call errors.init() t6                                       Memory
+	t8 = Store <mem> {int} i 42:int t7                               Memory
 	jump 2
 2:                                                            init.done P:2 S:0
 	return

ssa/const.go
@@ -9,7 +9,6 @@ package ssa
 import (
 	"fmt"
 	"go/constant"
-	"go/token"
 	"go/types"
 	"strconv"
 )
@@ -18,7 +17,12 @@ import (
 // val must be valid according to the specification of Const.Value.
 //
 func NewConst(val constant.Value, typ types.Type) *Const {
-	return &Const{typ: typ, Value: val}
+	return &Const{
+		register: register{
+			typ: typ,
+		},
+		Value: val,
+	}
 }
 
 // intConst returns an 'int' constant that evaluates to i.
@@ -88,26 +92,8 @@ func (c *Const) RelString(from *types.Package) string {
 	return s + ":" + relType(c.Type(), from)
 }
 
-func (c *Const) Name() string {
-	return c.RelString(nil)
-}
-
 func (c *Const) String() string {
-	return c.Name()
-}
-
-func (c *Const) Type() types.Type {
-	return c.typ
-}
-
-func (c *Const) Referrers() *[]Instruction {
-	return nil
-}
-
-func (c *Const) Parent() *Function { return nil }
-
-func (c *Const) Pos() token.Pos {
-	return token.NoPos
+	return c.RelString(nil)
 }
 
 // IsNil returns true if this constant represents a typed or untyped nil value.

ssa/example_test.go
@@ -91,11 +91,12 @@ func Example_buildPackage() {
 	// # Synthetic: package initializer
 	// func init():
 	// 0:                                                                entry P:0 S:2
-	// 	t1 = InitMem                                                     Memory
-	// 	t2 = Load <bool> init$guard t1                                     bool
-	// 	if t2 goto 2 else 1
+	// 	t1 = true:bool                                                     bool
+	// 	t2 = InitMem                                                     Memory
+	// 	t3 = Load <bool> init$guard t2                                     bool
+	// 	if t3 goto 2 else 1
 	// 1:                                                           init.start P:1 S:1
-	// 	t5 = Store <mem> {bool} init$guard true:bool t1                  Memory
+	// 	t5 = Store <mem> {bool} init$guard true:bool t2                  Memory
 	// 	t6 = call fmt.init() t5                                          Memory
 	// 	jump 2
 	// 2:                                                            init.done P:2 S:0
@@ -106,14 +107,16 @@ func Example_buildPackage() {
 	// # Location: hello.go:8:6
 	// func main():
 	// 0:                                                                entry P:0 S:0
-	// 	t1 = InitMem                                                     Memory
-	// 	t2 = new [1]interface{} (varargs)                       *[1]interface{}
-	// 	t3 = &t2[0:int]                                            *interface{}
-	// 	t5 = make interface{} <- string ("Hello, World!":string)    interface{}
-	// 	t7 = Store <mem> {interface{}} t3 t5 t1                          Memory
-	// 	t8 = slice t2[:]                                          []interface{}
+	// 	t1 = "Hello, World!":string                                      string
+	// 	t2 = 0:int                                                          int
+	// 	t3 = InitMem                                                     Memory
+	// 	t4 = new [1]interface{} (varargs)                       *[1]interface{}
+	// 	t5 = &t4[0:int]                                            *interface{}
+	// 	t6 = make interface{} <- string ("Hello, World!":string)    interface{}
+	// 	t7 = Store <mem> {interface{}} t5 t6 t3                          Memory
+	// 	t8 = slice t4[:]                                          []interface{}
 	// 	t9 = call fmt.Println(t8...) t7                                  Memory
-	// 	t11 = ReturnValues t9                                (n int, err error)
+	// 	t10 = ReturnValues t9                                (n int, err error)
 	// 	return
 }
 

ssa/func.go
@@ -179,14 +179,11 @@ func (f *Function) addParam(name string, typ types.Type, pos token.Pos) *Paramet
 		b = f.Blocks[0]
 	}
 	v := &Parameter{
-		anInstruction: anInstruction{
-			block: b,
-		},
-		name:   name,
-		typ:    typ,
-		pos:    pos,
-		parent: f,
+		name: name,
 	}
+	v.setBlock(b)
+	v.setType(typ)
+	v.setPos(pos)
 	f.Params = append(f.Params, v)
 	if b != nil {
 		// There may be no blocks if this function has no body. We
@@ -284,24 +281,10 @@ func numberNodes(f *Function) {
 	var base ID
 	for _, b := range f.Blocks {
 		for _, instr := range b.Instrs {
-			base = numberNode(instr.(Node), base)
+			base++
+			instr.setID(base)
 		}
 	}
-}
-
-func numberNode(node Node, base ID) ID {
-	if node.ID() != 0 {
-		return base
-	}
-	base++
-	node.setID(base)
-	ops := node.Operands(nil)
-	for _, op := range ops {
-		if *op != nil {
-			base = numberNode((*op).(Node), base)
-		}
-	}
-	return base
 }
 
 // buildReferrers populates the def/use information in all non-nil
@@ -325,6 +308,17 @@ func buildReferrers(f *Function) {
 
 // finishBody() finalizes the function after SSA code generation of its body.
 func (f *Function) finishBody() {
+	if len(f.Blocks) > 0 && len(f.consts) > 0 {
+		instrs := make([]Instruction, len(f.Blocks[0].Instrs)+len(f.consts))
+		copy(instrs, f.consts)
+		copy(instrs[len(f.consts):], f.Blocks[0].Instrs)
+		f.Blocks[0].Instrs = instrs
+		for _, c := range f.consts {
+			c.setBlock(f.Blocks[0])
+		}
+	}
+
+	f.consts = nil
 	f.objects = nil
 	f.currentBlock = nil
 	f.lblocks = nil
@@ -424,10 +418,19 @@ func (f *Function) finishBody() {
 	buildDomTree(f)
 
 	if f.Prog.mode&NaiveForm == 0 {
-		// For debugging pre-state of lifting pass:
-		// numberRegisters(f)
-		// f.WriteTo(os.Stderr)
 		lift(f)
+
+		// lifting may have produced more constants
+		if len(f.Blocks) > 0 && len(f.consts) > 0 {
+			instrs := make([]Instruction, len(f.Blocks[0].Instrs)+len(f.consts))
+			copy(instrs, f.consts)
+			copy(instrs[len(f.consts):], f.Blocks[0].Instrs)
+			f.Blocks[0].Instrs = instrs
+			for _, c := range f.consts {
+				c.setBlock(f.Blocks[0])
+			}
+		}
+		f.consts = nil
 	}
 
 	f.namedResults = nil // (used by lifting)

ssa/html.go
@@ -24,14 +24,6 @@ func live(f *Function) []bool {
 			if int(instr.ID()) > max {
 				max = int(instr.ID())
 			}
-
-			// TODO(dh): this is currently needed because not all
-			// operands show up in the instruction stream (in particular Const)
-			for _, op := range instr.Operands(ops) {
-				if int((*op).ID()) > max {
-					max = int((*op).ID())
-				}
-			}
 		}
 	}
 
@@ -51,6 +43,9 @@ func live(f *Function) []bool {
 		v := q[len(q)-1]
 		q = q[:len(q)-1]
 		for _, op := range v.Operands(ops) {
+			if *op == nil {
+				continue
+			}
 			if !out[(*op).ID()] {
 				out[(*op).ID()] = true
 				q = append(q, *op)
@@ -776,8 +771,6 @@ func ValueHTML(v Node) string {
 		label = v.Name()
 	case *Builtin:
 		label = v.Name()
-	case *Const:
-		label = v.String()
 	default:
 		label = class
 	}
@@ -807,6 +800,12 @@ func ValueLongHTML(v Node) string {
 		s += fmt.Sprintf(" {%s}", html.EscapeString(v.Op.String()))
 	case *UnOp:
 		s += fmt.Sprintf(" {%s}", html.EscapeString(v.Op.String()))
+	case *Const:
+		if v.Value == nil {
+			s += " {&lt;nil&gt;}"
+		} else {
+			s += fmt.Sprintf(" {%s}", html.EscapeString(v.Value.String()))
+		}
 	}
 	for _, a := range v.Operands(nil) {
 		s += fmt.Sprintf(" %s", ValueHTML(*a))

commit a2fc4037bf9fb9a7662bf915cde585ccc17c1c2b

ssa: don't crash when encountering nil const on lhs of comparison
This fixes a regression introduced in d48d6430c341431283deaf5ef49e345c4d34db16

Closes gh-613

ssa/emit.go
@@ -122,7 +122,7 @@ func emitCompare(f *Function, op token.Token, x, y Value, pos token.Pos) Value {
 	//   if e==true { ... }
 	// even in the case when e's type is an interface.
 	// TODO(adonovan): opt: generalise to x==true, false!=y, etc.
-	if x, ok := x.(*Const); ok && op == token.EQL && x.Value.Kind() == constant.Bool && constant.BoolVal(x.Value) {
+	if x, ok := x.(*Const); ok && op == token.EQL && x.Value != nil && x.Value.Kind() == constant.Bool && constant.BoolVal(x.Value) {
 		if yt, ok := yt.(*types.Basic); ok && yt.Info()&types.IsBoolean != 0 {
 			return y
 		}