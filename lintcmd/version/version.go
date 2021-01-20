341a1b14019c37b544a1f45db83ed3d4c76dfee1
Dominik Honnef <dominik@honnef.co>
2021-01-20 13:18:51 +0100

    Version 2020.2.1 (v0.1.1)

--- lintcmd/version/version.go
+++ lintcmd/version/version.go
@@ -7,8 +7,8 @@ import (
 	"runtime"
 )
 
-const Version = "2020.2"
-const MachineVersion = "v0.1.0"
+const Version = "2020.2.1"
+const MachineVersion = "v0.1.1"
 
 // version returns a version descriptor and reports whether the
 // version is a known release.
