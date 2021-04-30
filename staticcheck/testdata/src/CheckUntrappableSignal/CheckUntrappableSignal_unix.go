7b16c9f6ab25566117510e763894694608e1f6d6
Dominik Honnef <dominik@honnef.co>
2021-04-30 21:57:45 +0200

    staticcheck: use //go:build in fix textures to support Go 1.17

--- staticcheck/testdata/src/CheckUntrappableSignal/CheckUntrappableSignal_unix.go
+++ staticcheck/testdata/src/CheckUntrappableSignal/CheckUntrappableSignal_unix.go
@@ -1,3 +1,4 @@
+//go:build android || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
 // +build android darwin dragonfly freebsd linux netbsd openbsd solaris
 
 package main
