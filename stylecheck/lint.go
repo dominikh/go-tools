dominikh/go-tools

commit 72f0c2356d0aa21a18fa8dc24990c296f43dcdbc

stylecheck: don't complain about empty documentation
Our checks that validate documentation on exported objects only flag
malformed documentation, not missing documentation.

As a special case in (*ast.CommentGroup).Text, tool directives such as
//go:noinline get stripped from the documentation. This can lead to
documentation that is non-nil but empty, if the comments only
consisted of such directives.

However, we don't want to flag

    //tool:directive
	type X int

for having a malformed comment.

With this change, we will treat empty documentation the same as
missing documentation and ignore it. This does mean that we won't flag
the following:

    //
	type X int

That's really a side-effect of how we skip over directives in
comments, and arguably we might want to treat "just directives"
differently from "empty comment", but it is probably not worth the
added complexity.

stylecheck/lint.go
@@ -29,6 +29,14 @@ import (
 	"golang.org/x/tools/go/ast/inspector"
 )
 
+func docText(doc *ast.CommentGroup) (string, bool) {
+	if doc == nil {
+		return "", false
+	}
+	text := doc.Text()
+	return text, text != ""
+}
+
 func CheckPackageComment(pass *analysis.Pass) (interface{}, error) {
 	// - At least one file in a non-main package should have a package comment
 	//
@@ -46,10 +54,11 @@ func CheckPackageComment(pass *analysis.Pass) (interface{}, error) {
 		if code.IsInTest(pass, f) {
 			continue
 		}
-		if f.Doc != nil && len(f.Doc.List) > 0 {
+		text, ok := docText(f.Doc)
+		if ok {
 			hasDocs = true
 			prefix := "Package " + f.Name.Name + " "
-			if !strings.HasPrefix(strings.TrimSpace(f.Doc.Text()), prefix) {
+			if !strings.HasPrefix(text, prefix) {
 				report.Report(pass, f.Doc, fmt.Sprintf(`package comment should be of the form "%s..."`, prefix))
 			}
 		}
@@ -774,7 +783,8 @@ func CheckExportedFunctionDocs(pass *analysis.Pass) (interface{}, error) {
 		}
 
 		decl := node.(*ast.FuncDecl)
-		if decl.Doc == nil {
+		text, ok := docText(decl.Doc)
+		if !ok {
 			return
 		}
 		if !ast.IsExported(decl.Name.Name) {
@@ -797,7 +807,7 @@ func CheckExportedFunctionDocs(pass *analysis.Pass) (interface{}, error) {
 			}
 		}
 		prefix := decl.Name.Name + " "
-		if !strings.HasPrefix(decl.Doc.Text(), prefix) {
+		if !strings.HasPrefix(text, prefix) {
 			report.Report(pass, decl.Doc, fmt.Sprintf(`comment on exported %s %s should be of the form "%s..."`, kind, decl.Name.Name, prefix), report.FilterGenerated())
 		}
 	}
@@ -830,7 +840,8 @@ func CheckExportedTypeDocs(pass *analysis.Pass) (interface{}, error) {
 			}
 
 			doc := node.Doc
-			if doc == nil {
+			text, ok := docText(doc)
+			if !ok {
 				if len(genDecl.Specs) != 1 {
 					// more than one spec in the GenDecl, don't validate the
 					// docstring
@@ -841,12 +852,13 @@ func CheckExportedTypeDocs(pass *analysis.Pass) (interface{}, error) {
 					return false
 				}
 				doc = genDecl.Doc
-				if doc == nil {
+				text, ok = docText(doc)
+				if !ok {
 					return false
 				}
 			}
 
-			s := doc.Text()
+			s := text
 			articles := [...]string{"A", "An", "The"}
 			for _, a := range articles {
 				if strings.HasPrefix(s, a+" ") {
@@ -897,11 +909,12 @@ func CheckExportedVarDocs(pass *analysis.Pass) (interface{}, error) {
 			if !ast.IsExported(name) {
 				return false
 			}
-			if genDecl.Doc == nil {
+			text, ok := docText(genDecl.Doc)
+			if !ok {
 				return false
 			}
 			prefix := name + " "
-			if !strings.HasPrefix(genDecl.Doc.Text(), prefix) {
+			if !strings.HasPrefix(text, prefix) {
 				kind := "var"
 				if genDecl.Tok == token.CONST {
 					kind = "const"

stylecheck/testdata/src/CheckExportedFunctionDocs/CheckExportedFunctionDocs.go
@@ -19,3 +19,25 @@ func (T) Foo() {}
 
 // Whatever // want `comment on exported method`
 func (T) Bar() {}
+
+//
+func Qux() {} // this is fine, because "no comment" and "empty comment" are treated the same
+
+//
+// Meow is amazing.
+//
+// godoc allows this style, because ast.CommentGroup.Text strips whitespace.
+// We currently make no effort to flag it.
+//
+func Meow() {}
+
+//some:directive
+func F1() {} // we pretend that directives aren't part of the doc string, just like godoc in Go 1.15+ does
+
+//some:directive
+// F2 is amazing
+func F2() {}
+
+//some:directive // want `comment on exported function`
+// Whatever
+func F3() {}

stylecheck/testdata/src/CheckExportedTypeDocs/CheckExportedTypeDocs.go
@@ -33,3 +33,25 @@ func fn() {
 	// Some type
 	type T1 struct{}
 }
+
+//
+type T10 struct{} // this is fine, because "no comment" and "empty comment" are treated the same
+
+//
+// T11 is amazing.
+//
+// godoc allows this style, because ast.CommentGroup.Text strips whitespace.
+// We currently make no effort to flag it.
+//
+type T11 struct{}
+
+//some:directive
+type T12 struct{} // we pretend that directives aren't part of the doc string, just like godoc in Go 1.15+ does
+
+//some:directive
+// T13 is amazing
+type T13 struct{}
+
+//some:directive // want `comment on exported type`
+// Whatever
+type T14 struct{}

stylecheck/testdata/src/CheckExportedVarDocs/CheckExportedVarDocs.go
@@ -17,3 +17,25 @@ func fn() {
 	var D int
 	_ = D
 }
+
+//
+var E int // this is fine, because "no comment" and "empty comment" are treated the same
+
+//
+// F is amazing.
+//
+// godoc allows this style, because ast.CommentGroup.Text strips whitespace.
+// We currently make no effort to flag it.
+//
+var F int
+
+//some:directive
+var G int // we pretend that directives aren't part of the doc string, just like godoc in Go 1.15+ does
+
+//some:directive
+// H is amazing
+var H int
+
+//some:directive // want `should be of the form`
+// Whatever
+var I int

commit 3d8e6a730b44b8a2f6e399270d4045df3e240476

stylecheck: trim space of doc comments
Fixes a regression introduced in 72f0c2356d0aa21a18fa8dc24990c296f43dcdbc.

stylecheck/lint.go
@@ -33,7 +33,8 @@ func docText(doc *ast.CommentGroup) (string, bool) {
 	if doc == nil {
 		return "", false
 	}
-	text := doc.Text()
+	// We trim spaces primarily because of /**/ style comments, which often have leading space.
+	text := strings.TrimSpace(doc.Text())
 	return text, text != ""
 }
 

stylecheck/lint_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestAll(t *testing.T) {
 	checks := map[string][]testutil.Test{
-		"ST1000": {{Dir: "CheckPackageComment-1"}, {Dir: "CheckPackageComment-2"}},
+		"ST1000": {{Dir: "CheckPackageComment-1"}, {Dir: "CheckPackageComment-2"}, {Dir: "CheckPackageComment-3"}},
 		"ST1001": {{Dir: "CheckDotImports"}},
 		"ST1003": {{Dir: "CheckNames"}, {Dir: "CheckNames_generated"}},
 		"ST1005": {{Dir: "CheckErrorStrings"}},

stylecheck/testdata/src/CheckPackageComment-3/CheckPackageComment-3.go
@@ -1,0 +1,4 @@
+/*
+   Package pkg is useful.
+*/
+package pkg