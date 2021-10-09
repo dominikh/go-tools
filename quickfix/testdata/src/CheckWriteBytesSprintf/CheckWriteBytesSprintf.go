5335c16898c7d8bce572e125b49004125964a123
Dominik Honnef <dominik@honnef.co>
2021-10-09 18:23:28 +0200

    QF1012: fix tests that we broke in the previous commit

--- quickfix/testdata/src/CheckWriteBytesSprintf/CheckWriteBytesSprintf.go
+++ quickfix/testdata/src/CheckWriteBytesSprintf/CheckWriteBytesSprintf.go
@@ -14,9 +14,9 @@ func fn1() {
 	var w io.Writer
 	var w2 NotAWriter
 
-	w.Write([]byte(fmt.Sprint("abc", "de")))   // want `should use fmt.Fprint`
-	w.Write([]byte(fmt.Sprintf("%T", w)))      // want `should use fmt.Fprintf`
-	w.Write([]byte(fmt.Sprintln("abc", "de"))) // want `should use fmt.Fprintln`
+	w.Write([]byte(fmt.Sprint("abc", "de")))   // want `Use fmt.Fprint`
+	w.Write([]byte(fmt.Sprintf("%T", w)))      // want `Use fmt.Fprintf`
+	w.Write([]byte(fmt.Sprintln("abc", "de"))) // want `Use fmt.Fprintln`
 
 	w2.Write([]byte(fmt.Sprint("abc", "de")))
 }
@@ -25,9 +25,9 @@ func fn2() {
 	buf := new(bytes.Buffer)
 	var sw io.StringWriter
 
-	buf.WriteString(fmt.Sprint("abc", "de"))   // want `should use fmt.Fprint`
-	buf.WriteString(fmt.Sprintf("%T", 0))      // want `should use fmt.Fprintf`
-	buf.WriteString(fmt.Sprintln("abc", "de")) // want `should use fmt.Fprintln`
+	buf.WriteString(fmt.Sprint("abc", "de"))   // want `Use fmt.Fprint`
+	buf.WriteString(fmt.Sprintf("%T", 0))      // want `Use fmt.Fprintf`
+	buf.WriteString(fmt.Sprintln("abc", "de")) // want `Use fmt.Fprintln`
 
 	// We can't suggest fmt.Fprint here. We don't know if sw implements io.Writer.
 	sw.WriteString(fmt.Sprint("abc", "de"))
