2a0fc5c5553ad062967767384a3839ae6722f786
Dominik Honnef <dominik@honnef.co>
2021-11-06 06:49:04 +0100

    S1038: support functions from log and testing
    Extend S1038 to flag calls such as t.Error(fmt.Sprintf(...)) and
    log.Print(fmt.Sprintf(...)) in
    addition to fmt.Print(fmt.Sprintf(...)).
    
    Closes gh-997

--- simple/lint.go
+++ simple/lint.go
@@ -1822,23 +1822,80 @@ func CheckElaborateSleep(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
-var checkPrintSprintQ = pattern.MustParse(`
-	(Or
+var (
+	checkPrintSprintQ = pattern.MustParse(`
+		(Or
+			(CallExpr
+				fn@(Or
+					(Function "fmt.Print")
+					(Function "fmt.Sprint")
+					(Function "fmt.Println")
+					(Function "fmt.Sprintln"))
+				[(CallExpr (Function "fmt.Sprintf") f:_)])
+			(CallExpr
+				fn@(Or
+					(Function "fmt.Fprint")
+					(Function "fmt.Fprintln"))
+				[_ (CallExpr (Function "fmt.Sprintf") f:_)]))`)
+
+	checkTestingErrorSprintfQ = pattern.MustParse(`
 		(CallExpr
-			fn@(Or
-				(Function "fmt.Print")
-				(Function "fmt.Sprint")
-				(Function "fmt.Println")
-				(Function "fmt.Sprintln"))
-			[(CallExpr (Function "fmt.Sprintf") f:_)])
+			sel@(SelectorExpr
+				recv
+				(Ident
+					name@(Or
+						"Error"
+						"Fatal"
+						"Fatalln"
+						"Log"
+						"Panic"
+						"Panicln"
+						"Print"
+						"Println"
+						"Skip")))
+			[(CallExpr (Function "fmt.Sprintf") args)])`)
+
+	checkLogSprintfQ = pattern.MustParse(`
 		(CallExpr
-			fn@(Or
-				(Function "fmt.Fprint")
-				(Function "fmt.Fprintln"))
-			[_ (CallExpr (Function "fmt.Sprintf") f:_)]))`)
+			(Function
+				(Or
+					"log.Fatal"
+					"log.Fatalln"
+					"log.Panic"
+					"log.Panicln"
+					"log.Print"
+					"log.Println"))
+			[(CallExpr (Function "fmt.Sprintf") args)])`)
+
+	checkSprintfMapping = map[string]struct {
+		recv        string
+		alternative string
+	}{
+		"(*testing.common).Error": {"(*testing.common)", "Errorf"},
+		"(testing.TB).Error":      {"(testing.TB)", "Errorf"},
+		"(*testing.common).Fatal": {"(*testing.common)", "Fatalf"},
+		"(testing.TB).Fatal":      {"(testing.TB)", "Fatalf"},
+		"(*testing.common).Log":   {"(*testing.common)", "Logf"},
+		"(testing.TB).Log":        {"(testing.TB)", "Logf"},
+		"(*testing.common).Skip":  {"(*testing.common)", "Skipf"},
+		"(testing.TB).Skip":       {"(testing.TB)", "Skipf"},
+		"(*log.Logger).Fatal":     {"(*log.Logger)", "Fatalf"},
+		"(*log.Logger).Fatalln":   {"(*log.Logger)", "Fatalf"},
+		"(*log.Logger).Panic":     {"(*log.Logger)", "Panicf"},
+		"(*log.Logger).Panicln":   {"(*log.Logger)", "Panicf"},
+		"(*log.Logger).Print":     {"(*log.Logger)", "Printf"},
+		"(*log.Logger).Println":   {"(*log.Logger)", "Printf"},
+		"log.Fatal":               {"", "log.Fatalf"},
+		"log.Fatalln":             {"", "log.Fatalf"},
+		"log.Panic":               {"", "log.Panicf"},
+		"log.Panicln":             {"", "log.Panicf"},
+		"log.Print":               {"", "log.Printf"},
+		"log.Println":             {"", "log.Printf"},
+	}
+)
 
 func CheckPrintSprintf(pass *analysis.Pass) (interface{}, error) {
-	fn := func(node ast.Node) {
+	fmtPrintf := func(node ast.Node) {
 		m, ok := code.Match(pass, checkPrintSprintQ, node)
 		if !ok {
 			return
@@ -1864,6 +1921,54 @@ func CheckPrintSprintf(pass *analysis.Pass) (interface{}, error) {
 		}
 		report.Report(pass, node, msg,
 			report.FilterGenerated())
+	}
+
+	methSprintf := func(node ast.Node) {
+		m, ok := code.Match(pass, checkTestingErrorSprintfQ, node)
+		if !ok {
+			return
+		}
+		mapped, ok := checkSprintfMapping[code.CallName(pass, node.(*ast.CallExpr))]
+		if !ok {
+			return
+		}
+
+		// Ensure that Errorf/Fatalf refer to the right method
+		recvTV := pass.TypesInfo.Types[m.State["recv"].(ast.Expr)]
+		obj, _, _ := types.LookupFieldOrMethod(recvTV.Type, recvTV.Addressable(), nil, mapped.alternative)
+		f, ok := obj.(*types.Func)
+		if !ok {
+			return
+		}
+		if typeutil.FuncName(f) != mapped.recv+"."+mapped.alternative {
+			return
+		}
+
+		alt := &ast.SelectorExpr{
+			X:   m.State["recv"].(ast.Expr),
+			Sel: &ast.Ident{Name: mapped.alternative},
+		}
+		report.Report(pass, node, fmt.Sprintf("should use %s(...) instead of %s(fmt.Sprintf(...))", report.Render(pass, alt), report.Render(pass, m.State["sel"].(*ast.SelectorExpr))))
+	}
+
+	pkgSprintf := func(node ast.Node) {
+		_, ok := code.Match(pass, checkLogSprintfQ, node)
+		if !ok {
+			return
+		}
+		callName := code.CallName(pass, node.(*ast.CallExpr))
+		mapped, ok := checkSprintfMapping[callName]
+		if !ok {
+			return
+		}
+		report.Report(pass, node, fmt.Sprintf("should use %s(...) instead of %s(fmt.Sprintf(...))", mapped.alternative, callName))
+	}
+
+	fn := func(node ast.Node) {
+		fmtPrintf(node)
+		// TODO(dh): add suggested fixes
+		methSprintf(node)
+		pkgSprintf(node)
 	}
 	code.Preorder(pass, fn, (*ast.CallExpr)(nil))
 	return nil, nil
--- simple/testdata/src/CheckPrintSprintf/CheckPrintSprintf.go
+++ simple/testdata/src/CheckPrintSprintf/CheckPrintSprintf.go
@@ -1,6 +1,10 @@
 package pkg
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"testing"
+)
 
 func fn() {
 	fmt.Print(fmt.Sprintf("%d", 1))         // want `should use fmt\.Printf`
@@ -12,4 +16,85 @@ func fn() {
 
 	arg := "%d"
 	fmt.Println(fmt.Sprintf(arg, 1))
+}
+
+func Sprintf(string) string { return "" }
+
+type Embedding1 struct {
+	*testing.T
+}
+
+type Embedding2 struct {
+	*testing.T
+}
+
+func (e Embedding2) Errorf() {}
+
+type Embedding3 struct {
+	*testing.T
+}
+
+func (e Embedding3) Error(string, ...interface{}) {}
+
+type Embedding4 struct {
+	testing.TB
+}
+
+func fn2() {
+	var t *testing.T
+	var b *testing.B
+	var tb testing.TB
+
+	// All of these are the basic cases that should be flagged
+	t.Error(fmt.Sprintf(""))  // want `use t\.Errorf\(\.\.\.\) instead of t\.Error\(fmt\.Sprintf\(\.\.\.\)\)`
+	b.Error(fmt.Sprintf(""))  // want `use b\.Errorf`
+	tb.Error(fmt.Sprintf("")) // want `use tb\.Errorf`
+	t.Fatal(fmt.Sprintf(""))  // want `use t\.Fatalf`
+	b.Fatal(fmt.Sprintf(""))  // want `use b\.Fatalf`
+	tb.Fatal(fmt.Sprintf("")) // want `use tb\.Fatalf`
+	t.Log(fmt.Sprintf(""))    // want `use t\.Logf`
+	b.Log(fmt.Sprintf(""))    // want `use b\.Logf`
+	tb.Log(fmt.Sprintf(""))   // want `use tb\.Logf`
+	t.Skip(fmt.Sprintf(""))   // want `use t\.Skipf`
+	b.Skip(fmt.Sprintf(""))   // want `use b\.Skipf`
+	tb.Skip(fmt.Sprintf(""))  // want `use tb\.Skipf`
+
+	var e1 Embedding1
+	var e2 Embedding2
+	var e3 Embedding3
+	var e4 Embedding4
+	// Error and Errorf are both of *testing.common -> flag
+	e1.Error(fmt.Sprintf("")) // want `use e1\.Errorf`
+	// Fatal and Fatalf are both of *testing.common -> flag
+	e1.Fatal(fmt.Sprintf("")) // want `use e1\.Fatalf`
+	// Error is of *testing.common, but Errorf is Embedding2.Errorf -> don't flag
+	e2.Error(fmt.Sprintf(""))
+	// Fatal and Fatalf are both of *testing.common -> flag
+	e2.Fatal(fmt.Sprintf("")) // want `use e2\.Fatalf`
+	// Error is Embedding3.Error and Errorf is of *testing.common -> don't flag
+	e3.Error(fmt.Sprintf(""))
+	// Fatal and Fatalf are both of *testing.common -> flag
+	e3.Fatal(fmt.Sprintf("")) // want `use e3\.Fatalf`
+	// Error and Errorf are both of testing.TB -> flag
+	e4.Error(fmt.Sprintf("")) // want `use e4\.Errorf`
+	// Fatal and Fatalf are both of testing.TB -> flag
+	e4.Fatal(fmt.Sprintf("")) // want `use e4\.Fatalf`
+
+	// Basic cases
+	log.Fatal(fmt.Sprintf(""))   // want `use log.Fatalf`
+	log.Fatalln(fmt.Sprintf("")) // want `use log.Fatalf`
+	log.Panic(fmt.Sprintf(""))   // want `use log.Panicf`
+	log.Panicln(fmt.Sprintf("")) // want `use log.Panicf`
+	log.Print(fmt.Sprintf(""))   // want `use log.Printf`
+	log.Println(fmt.Sprintf("")) // want `use log.Printf`
+
+	var l *log.Logger
+	l.Fatal(fmt.Sprintf(""))   // want `use l.Fatalf\(\.\.\.\) instead of l\.Fatal\(fmt\.Sprintf\(\.\.\.\)`
+	l.Fatalln(fmt.Sprintf("")) // want `use l.Fatalf`
+	l.Panic(fmt.Sprintf(""))   // want `use l.Panicf`
+	l.Panicln(fmt.Sprintf("")) // want `use l.Panicf`
+	l.Print(fmt.Sprintf(""))   // want `use l.Printf`
+	l.Println(fmt.Sprintf("")) // want `use l.Printf`
+
+	// log.Logger and testing.T share a code path, no need to check embedding again
 }
