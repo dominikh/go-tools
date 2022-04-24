443a9939b282ac3d97c9840fac1d9523e6f0aaf2
Dominik Honnef <dominik@honnef.co>
2022-04-24 08:10:17 +0200

    ST1015: don't reorder cases around fallthrough statements
    Closes gh-1188

--- stylecheck/testdata/src/CheckDefaultCaseOrder/CheckDefaultCaseOrder.go
+++ stylecheck/testdata/src/CheckDefaultCaseOrder/CheckDefaultCaseOrder.go
@@ -33,4 +33,52 @@ func fn(x int) {
 	default: //@ diag(`default case should be first or last in switch statement`)
 	case 2:
 	}
+
+	// Don't flag either of these two; fallthrough is sensitive to the order of cases
+	switch x {
+	case 1:
+		fallthrough
+	default:
+	case 2:
+	}
+	switch x {
+	case 1:
+	default:
+		fallthrough
+	case 2:
+	}
+
+	// Do flag these branches; don't get confused by the nested switch statements
+	switch x {
+	case 1:
+		func() {
+			switch x {
+			case 1:
+				fallthrough
+			case 2:
+			}
+		}()
+	default: //@ diag(`default case should be first or last in switch statement`)
+	case 3:
+	}
+
+	switch x {
+	case 1:
+		switch x {
+		case 1:
+			fallthrough
+		case 2:
+		}
+	default: //@ diag(`default case should be first or last in switch statement`)
+	case 3:
+	}
+
+	// Fallthrough may be followed by empty statements
+	switch x {
+	case 1:
+		fallthrough
+		;
+	default:
+	case 3:
+	}
 }
