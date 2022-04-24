f125e93a5c9bbe3640a07a57f56c777454d68630
Dominik Honnef <dominik@honnef.co>
2022-04-24 13:41:21 +0200

    SA1013: more robustly match constants
    Updates gh-1213
    
    (cherry picked from commit 3f103b8051389e875a548b426b02d142a73bff0b)

--- staticcheck/testdata/src/checkStdlibUsageSeeker/checkStdlibUsageSeeker.go
+++ staticcheck/testdata/src/checkStdlibUsageSeeker/checkStdlibUsageSeeker.go
@@ -1,12 +1,20 @@
 package pkg
 
-import "io"
+import (
+	"io"
+	myio "io"
+	"os"
+)
 
 func fn() {
 	const SeekStart = 0
 	var s io.Seeker
 	s.Seek(0, 0)
 	s.Seek(0, io.SeekStart)
-	s.Seek(io.SeekStart, 0) //@ diag(`the first argument of io.Seeker is the offset`)
+	s.Seek(io.SeekStart, 0)   //@ diag(`the first argument of io.Seeker is the offset`)
+	s.Seek(myio.SeekStart, 0) //@ diag(`the first argument of io.Seeker is the offset`)
 	s.Seek(SeekStart, 0)
+
+	var f *os.File
+	f.Seek(io.SeekStart, 0) //@ diag(`the first argument of io.Seeker is the offset`)
 }
