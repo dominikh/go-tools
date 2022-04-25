5468b56022af4534224b427607cd827e7f0a20b3
Dominik Honnef <dominik@honnef.co>
2022-04-25 05:05:02 +0200

    SA1013: ensure we're calling a method with the correct signature
    Closes gh-1213
    
    (cherry picked from commit 9a9acf1071850aa086d18468a7e02eac0533d076)

--- staticcheck/testdata/src/checkStdlibUsageSeeker/checkStdlibUsageSeeker.go
+++ staticcheck/testdata/src/checkStdlibUsageSeeker/checkStdlibUsageSeeker.go
@@ -6,6 +6,13 @@ import (
 	"os"
 )
 
+type T struct{}
+
+func (T) Seek(whence int, offset int64) (int64, error) {
+	// This method does NOT implement io.Seeker
+	return 0, nil
+}
+
 func fn() {
 	const SeekStart = 0
 	var s io.Seeker
@@ -17,4 +24,7 @@ func fn() {
 
 	var f *os.File
 	f.Seek(io.SeekStart, 0) //@ diag(`the first argument of io.Seeker is the offset`)
+
+	var t T
+	t.Seek(io.SeekStart, 0) // not flagged, T is not an io.Seeker
 }
