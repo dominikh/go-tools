7586196b2bb05a248f1934d8087c759d05910e60
Dominik Honnef <dominik@honnef.co>
2022-05-17 13:24:40 +0200

    Version 2022.1.2 (v0.3.2)

--- lintcmd/version/version.go
+++ lintcmd/version/version.go
@@ -7,8 +7,8 @@ import (
 	"runtime"
 )
 
-const Version = "2022.1.1"
-const MachineVersion = "v0.3.1"
+const Version = "2022.1.2"
+const MachineVersion = "v0.3.2"
 
 // version returns a version descriptor and reports whether the
 // version is a known release.
