98267841da4f9703bb19aba8434008f511949123
Dominik Honnef <dominik@honnef.co>
2023-01-19 12:45:04 +0100

    website: add npm install to build steps
    Docsy no longer includes bootstrap and font-awesome as git submodules,
    instead relying on npm.

--- website/build.sh
+++ website/build.sh
@@ -3,4 +3,11 @@ set -e
 rm -rf ./public
 go run ./cmd/generate_checks/generate_checks.go >data/checks.json
 go run ./cmd/generate_config/generate_config.go >content/docs/configuration/default_config/index.md
+
+(
+	cd themes/docsy
+	# --omit=dev so we don't try to install Hugo as an NPM module
+	npm install --omit=dev
+)
+
 hugo --minify
