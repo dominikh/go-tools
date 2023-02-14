22a3f20c0f81249f7764195666e837a75fe4f0aa
Dominik Honnef <dominik@honnef.co>
2023-02-14 11:24:28 +0100

    Version 2023.1.1 (v0.4.1)

--- lintcmd/version/version.go
+++ lintcmd/version/version.go
@@ -7,8 +7,8 @@ import (
 	"runtime"
 )
 
-const Version = "2023.1"
-const MachineVersion = "v0.4.0"
+const Version = "2023.1.1"
+const MachineVersion = "v0.4.1"
 
 // version returns a version descriptor and reports whether the
 // version is a known release.
