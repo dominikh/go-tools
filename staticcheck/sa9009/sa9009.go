909a365b0eee6a2a3ea0395e6574149e06d6819b
Eric Chlebek <eric@sensu.io>
2023-12-13 13:45:50 -0800

    Add SA9009
    This commit adds SA9009, a check that looks for ineffectual Go compiler
    directives. The check flags comments that appear to be valid Go compiler
    directives, but have tabs or spaces between the start of the comment
    block and the directive itself.
    
    Signed-off-by: Eric Chlebek <eric@sensu.io>

---
+++ staticcheck/sa9009/sa9009.go
@@ -0,0 +1,45 @@
+package sa9009
+
+import (
+	"fmt"
+	"regexp"
+
+	"honnef.co/go/tools/analysis/lint"
+	"honnef.co/go/tools/analysis/report"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var SCAnalyzer = lint.InitializeAnalyzer(&lint.Analyzer{
+	Analyzer: &analysis.Analyzer{
+		Name:     "SA9009",
+		Run:      run,
+		Requires: []*analysis.Analyzer{},
+	},
+	Doc: &lint.Documentation{
+		Title: "ineffectual go compiler directive",
+		Text: `
+A go compiler directive was found, but is ineffectual as it begins
+with whitespace.`,
+		Since:    "Unreleased",
+		Severity: lint.SeverityWarning,
+	},
+})
+
+var Analyzer = SCAnalyzer.Analyzer
+
+func run(pass *analysis.Pass) (any, error) {
+	re := regexp.MustCompile("[ \t]*(//|/\\*)[ \t]+go:")
+	for _, f := range pass.Files {
+		for _, cg := range f.Comments {
+			for _, c := range cg.List {
+				if re.FindStringIndex(c.Text) == nil {
+					continue
+				}
+				report.Report(pass, c,
+					fmt.Sprintf("ineffectual compiler directive: %q", c.Text))
+			}
+		}
+	}
+	return nil, nil
+}
