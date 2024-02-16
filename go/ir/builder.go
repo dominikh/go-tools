d965e98a28ee8e5aa3d84ecf28bd724a2e643ea3
Dominik Honnef <dominik@honnef.co>
2024-02-16 19:26:02 +0100

    go/ir: emit Low expression before High in *ast.Slice

--- go/ir/builder.go
+++ go/ir/builder.go
@@ -680,11 +680,11 @@ func (b *builder) expr0(fn *Function, e ast.Expr, tv types.TypeAndValue) Value {
 		}
 
 		var low, high, max Value
-		if e.High != nil {
-			high = b.expr(fn, e.High)
-		}
 		if e.Low != nil {
 			low = b.expr(fn, e.Low)
+		}
+		if e.High != nil {
+			high = b.expr(fn, e.High)
 		}
 		if e.Slice3 {
 			max = b.expr(fn, e.Max)
