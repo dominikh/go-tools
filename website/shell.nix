6e586324bc8411106679d9ff57fec599bd9ee15d
Dominik Honnef <dominik@honnef.co>
2024-07-01 02:50:38 +0200

    website: only use shell.nix for postcss and Node, not Go

--- website/shell.nix
+++ website/shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
-  nativeBuildInputs = with pkgs; [ go_1_19 hugo postcss-cli nodejs ];
+  nativeBuildInputs = with pkgs; [ postcss-cli nodejs ];
 }
