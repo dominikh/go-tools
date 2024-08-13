d00dea326094e858d0c1efd894052ffc265093c2
Dominik Honnef <dominik@honnef.co>
2024-08-13 19:22:58 +0200

    website: ensure destination folders exist

--- website/build.sh
+++ website/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 rm -rf ./public
+mkdir -p data
+mkdir -p content/docs/configuration/default_config
 go run ./cmd/generate_checks/generate_checks.go >data/checks.json
 go run ./cmd/generate_config/generate_config.go >content/docs/configuration/default_config/index.md
 
