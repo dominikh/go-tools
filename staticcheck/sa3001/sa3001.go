3043378f98915c553f75f981271dd1eaa2cffc3a
Dominik Honnef <dominik@honnef.co>
2025-05-23 02:56:00 +0200

    SA3001: reimplement using pattern matching

--- staticcheck/sa3001/sa3001.go
+++ staticcheck/sa3001/sa3001.go
@@ -7,16 +7,16 @@ import (
 	"honnef.co/go/tools/analysis/code"
 	"honnef.co/go/tools/analysis/lint"
 	"honnef.co/go/tools/analysis/report"
+	"honnef.co/go/tools/pattern"
 
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/inspect"
 )
 
 var SCAnalyzer = lint.InitializeAnalyzer(&lint.Analyzer{
 	Analyzer: &analysis.Analyzer{
 		Name:     "SA3001",
 		Run:      run,
-		Requires: []*analysis.Analyzer{inspect.Analyzer},
+		Requires: code.RequiredAnalyzers,
 	},
 	Doc: &lint.RawDocumentation{
 		Title: `Assigning to \'b.N\' in benchmarks distorts the results`,
@@ -32,24 +32,16 @@ falsify results.`,
 
 var Analyzer = SCAnalyzer.Analyzer
 
+var query = pattern.MustParse(`(AssignStmt sel@(SelectorExpr selX (Ident "N")) "=" [_] )`)
+
 func run(pass *analysis.Pass) (any, error) {
-	fn := func(node ast.Node) {
+	for node, m := range code.Matches(pass, query) {
 		assign := node.(*ast.AssignStmt)
-		if len(assign.Lhs) != 1 || len(assign.Rhs) != 1 {
-			return
+		if !code.IsOfPointerToTypeWithName(pass, m.State["selX"].(ast.Expr), "testing.B") {
+			continue
 		}
-		sel, ok := assign.Lhs[0].(*ast.SelectorExpr)
-		if !ok {
-			return
-		}
-		if sel.Sel.Name != "N" {
-			return
-		}
-		if !code.IsOfPointerToTypeWithName(pass, sel.X, "testing.B") {
-			return
-		}
-		report.Report(pass, assign, fmt.Sprintf("should not assign to %s", report.Render(pass, sel)))
+		report.Report(pass, assign,
+			fmt.Sprintf("should not assign to %s", report.Render(pass, m.State["sel"])))
 	}
-	code.Preorder(pass, fn, (*ast.AssignStmt)(nil))
 	return nil, nil
 }
