dee2ae65766651d8f37ceb0c42387ffc2f0bed5e
Dominik Honnef <dominik@honnef.co>
2026-01-08 23:23:42 +0100

    ST1019: allow a normal and a blank import of the same package
    Closes: gh-1688

--- stylecheck/st1019/st1019.go
+++ stylecheck/st1019/st1019.go
@@ -27,7 +27,6 @@ bit of code is valid:
         "fmt"
         fumpt "fmt"
         format "fmt"
-        _ "fmt"
     )
 
 However, this is very rarely done on purpose. Usually, it is a
@@ -39,7 +38,12 @@ Do note that sometimes, this feature may be used
 intentionally (see for example
 https://github.com/golang/go/commit/3409ce39bfd7584523b7a8c150a310cea92d879d)
 – if you want to allow this pattern in your code base, you're
-advised to disable this check.`,
+advised to disable this check.
+
+It is acceptable to import the same package twice if one of the imports
+uses the blank identifier. This is allowed in order to increase
+resilience against erroneous changes when using the same package for its
+side effects as well as its exported API.`,
 		Since:   "2020.1",
 		MergeIf: lint.MergeIfAny,
 	},
@@ -52,17 +56,17 @@ func run(pass *analysis.Pass) (any, error) {
 		// Collect all imports by their import path
 		imports := make(map[string][]*ast.ImportSpec, len(f.Imports))
 		for _, imp := range f.Imports {
+			if imp.Name != nil && imp.Name.Name == "_" {
+				// Allow blank imports to coexist with one normal import.
+				//
+				// We don't have to count the number of blank imports,
+				// goimports removes duplicates.
+				continue
+			}
 			imports[imp.Path.Value] = append(imports[imp.Path.Value], imp)
 		}
 
 		for path, value := range imports {
-			if path[1:len(path)-1] == "unsafe" {
-				// Don't flag unsafe. Cgo generated code imports
-				// unsafe using the blank identifier, and most
-				// user-written cgo code also imports unsafe
-				// explicitly.
-				continue
-			}
 			// If there's more than one import per path, we flag that
 			if len(value) > 1 {
 				s := fmt.Sprintf("package %s is being imported more than once", path)
--- stylecheck/st1019/testdata/go1.0/CheckDuplicatedImports/CheckDuplicatedImports.go
+++ stylecheck/st1019/testdata/go1.0/CheckDuplicatedImports/CheckDuplicatedImports.go
@@ -13,6 +13,17 @@ import (
 
 	"C"
 	_ "unsafe"
+
+	// This imports the package for its side effects, and then again to use it.
+	// If we flagged this, the user would have to remove the _ import. But if
+	// later the user stopped using the package directly, they'd be prone to
+	// removing the import, losing its side effects.
+	"net/http/pprof"
+	_ "net/http/pprof"
+
+	_ "strconv"
+	strconv1 "strconv" //@ diag(`package "strconv" is being imported more than once`)
+	strconv2 "strconv"
 )
 
 var _ = fmt.Println
@@ -21,3 +32,6 @@ var _ = fmt2.Println
 var _ = fine.ListenAndServe
 var _ = os.Getenv
 var _ = os1.Getenv
+var _ = pprof.Cmdline
+var _ = strconv1.AppendBool
+var _ = strconv2.AppendBool
